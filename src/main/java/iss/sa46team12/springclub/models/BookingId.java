104b87e67ba8627ea37c80ee92ffd523da8e2339
Chirag Shetty <e0283982@u.nus.edu>
2018-06-10 10:56:04 +0530

    Models

---
+++ src/main/java/iss/sa46team12/springclub/models/BookingId.java
@@ -0,0 +1,73 @@
+package iss.sa46team12.springclub.models;
+
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ * BookingId class
+ * Creating composite key for the bookingdetails table
+ * @version $Revision: 1.0
+ */
+@Embeddable
+public class BookingId {
+	/**Attributes for BookingId**/
+	@Column(name = "bookingid")
+	private int bookingId;
+	@Column(name = "facilityid")
+	private int facilityId;
+	@Column(name = "timeslotid")
+	private int timeslotId;
+	public int getBookingId() {
+		return bookingId;
+	}
+	/**Constructor**/
+	public BookingId(int bookingId, int facilityId, int timeslotId) {
+		super();
+		this.bookingId = bookingId;
+		this.facilityId = facilityId;
+		this.timeslotId = timeslotId;
+	}
+	/**Getters / Setters**/
+	public void setBookingId(int bookingId) {
+		this.bookingId = bookingId;
+	}
+	public int getFacilityId() {
+		return facilityId;
+	}
+	public void setFacilityId(int facilityId) {
+		this.facilityId = facilityId;
+	}
+	public int getTimeslotId() {
+		return timeslotId;
+	}
+	public void setTimeslotId(int timeslotId) {
+		this.timeslotId = timeslotId;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + bookingId;
+		result = prime * result + facilityId;
+		result = prime * result + timeslotId;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BookingId other = (BookingId) obj;
+		if (bookingId != other.bookingId)
+			return false;
+		if (facilityId != other.facilityId)
+			return false;
+		if (timeslotId != other.timeslotId)
+			return false;
+		return true;
+	}
+}
---
+++ src/main/java/iss/sa46team12/springclub/models/Timeslots.java
@@ -0,0 +1,47 @@
+package iss.sa46team12.springclub.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ * Timeslot class
+ *
+ * @version $Revision: 1.0
+ */
+@Entity
+@Table(name = "timeslots")
+public class Timeslots {
+	/** Attributes  **/
+	@Id
+	@Column (name ="timeslotid")
+	private int timeslotid;
+	@Column (name ="time")
+	private int time;
+	/** Mapping **/
+	@OneToMany(mappedBy="timeslots", cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+	private List<BookingDetails> bookingdetails;
+	
+	public List<BookingDetails> getBookingdetails() {
+		return (new ArrayList<BookingDetails> (bookingdetails));
+	}
+	/** Constructor **/
+	public Timeslots() {
+		super();
+	}
+	/** Getter Setter **/
+	public int getTimeslotid() {
+		return timeslotid;
+	}
+	public int getTime() {
+		return time;
+	}
+	
+}
