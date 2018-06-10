b86e0cb37e40bfc0f94744564fd19da3faaad1ae
Jayden <wwj.jayden@gmail.com>
2018-06-10 23:01:48 +0800

    Classes skeleton

--- src/main/java/iss/sa46team12/springclub/services/LoginService.java
+++ src/main/java/iss/sa46team12/springclub/services/LoginService.java
@@ -1,21 +1,9 @@
 package iss.sa46team12.springclub.services;
 
-import org.springframework.stereotype.Service;
+public interface LoginService {
+	public String showloginpg();
+	public String handleloginrequest();
+	public boolean validateuser(String user, String password);
+	public String checkuserrole();	
 
-@Service
-public class LoginService {
-	
-	public boolean validateuser(String user, String password) {
-		// if method to see if user role is active
-		return user.equalsIgnoreCase("frm db") && password.equals("frm db too");
-	}
-	
-	public String checkuserrole(int id) {
-//		if("some method to check ID .admin") {
-//			return "member";
-//		}
-//		else
-		return "admin";
-	}
-	
 }
