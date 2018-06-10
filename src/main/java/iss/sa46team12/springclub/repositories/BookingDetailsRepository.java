71884a9e5a6f33147970da086ac2a7ebf088d060
ZMH <zinminht3t@gmail.com>
2018-06-10 21:28:15 +0800

    Revert "Merge branch 'master' of https://github.com/zinminht3t/SA46Team12CABProject"
    This reverts commit 177b79131962a89de0ea65496b5fa59df6fb6ed5, reversing
    changes made to 4b09565559308e416b883067b77b10e6ae4ce255.

--- src/main/java/iss/sa46team12/springclub/repositories/BookingDetailsRepository.java
+++
@@ -1,8 +0,0 @@
-package iss.sa46team12.springclub.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import iss.sa46team12.springclub.models.BookingDetails;
-
-public interface BookingDetailsRepository extends JpaRepository<BookingDetails, Integer> {
-}
