12e009f3b0dbc3980fc36f612f5a0d8ef50fbe76
ZMH <zinminht3t@gmail.com>
2018-06-11 14:56:22 +0800

    Move Faciltiy and User CRUD to Admin Controller

--- src/main/java/iss/sa46team12/springclub/controllers/UserController.java
+++ src/main/java/iss/sa46team12/springclub/controllers/UserController.java
@@ -1,13 +1,16 @@
-
 package iss.sa46team12.springclub.controllers;
 
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,65 +21,51 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import iss.sa46team12.springclub.models.User;
 import iss.sa46team12.springclub.services.UserService;
 
-@RequestMapping("/admin/user")
+@RequestMapping(value = "/user")
 @Controller
 public class UserController {
 
+
+
 	@Autowired
-	UserService userService;
+	UserService uService;
 
-	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	public ModelAndView listAll() {
-		ModelAndView mav = new ModelAndView("UserList");
-		ArrayList<User> userlist = userService.findAllUsers();
-		mav.addObject("userlist", userlist);
+	@InitBinder
+	public void initBinder(final WebDataBinder binder) {
+		final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+	}
+
+	@RequestMapping(value = "/profile", method = RequestMethod.GET)
+	public ModelAndView editProfile() {
+		ModelAndView mav = new ModelAndView("userprofile", "changeUserPassword", new User());
+		User userProfile = uService.findUserById(1);
+		mav.addObject("userProfile", userProfile);
 		return mav;
 	}
 
-	@RequestMapping(value = "/edit/{nric}", method = RequestMethod.GET)
-	public ModelAndView editUserPage(@PathVariable String nric) {
-		ModelAndView mav = new ModelAndView("UserFormEdit");
-		// mav.addObject("user", userService.findUserByNric(nric));
-		return mav;
-	}
-
-	@RequestMapping(value = "/edit/{nric}", method = RequestMethod.POST)
-	public ModelAndView editUser(@ModelAttribute @Valid User user, @PathVariable String nric,
-			BindingResult result, final RedirectAttributes redirectAttributes) {		
+	@RequestMapping(value = "/profile/edit/{id}", method = RequestMethod.POST)
+	public ModelAndView editUser(@ModelAttribute @Valid User user, BindingResult result, @PathVariable String id,
+			final RedirectAttributes redirectAttributes) {
+		ModelAndView mav;
 		if (result.hasErrors())
-			return new ModelAndView("UserFormEdit");
-		userService.editUser(user);
-		ModelAndView mav = new ModelAndView("redirect:/admin/user/edit");
-		String message = "User" + user.getUserId() + " was successfully updated.";		
+			return new ModelAndView("userprofile");
+
+		mav = new ModelAndView("redirect:/user/profile");
+
+		User up = uService.findUserById(1);
+		up.setAddress(user.getAddress());
+		up.setContactno(user.getContactno());
+		up.setFullname(user.getFullname());
+		up.setDateofbirth(user.getDateofbirth());
+		up.setEmail(user.getEmail());
+		up.setGender(user.getGender());
+		up.setNric(user.getNric());
+
+		String message = "User was successfully updated.";
+		//uService.changeProfile(up);
+
+		redirectAttributes.addFlashAttribute("message", message);
 		return mav;
 	}
-
 }
-// package iss.sa46team12.springclub.controllers;
-// import java.util.ArrayList;
-//
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Controller;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestMethod;
-// import org.springframework.web.servlet.ModelAndView;
-//
-// import iss.sa46team12.springclub.models.User;
-// import iss.sa46team12.springclub.services.UserService;
-//
-//
-// @RequestMapping(value="/user")
-// @Controller
-// public class UserController {
-//
-// @Autowired
-// UserService uService;
-//
-// @RequestMapping(value = "/userprofile", method = RequestMethod.GET)
-// public ModelAndView listAll() {
-// ModelAndView mav = new ModelAndView("UserProfile");
-// User userProfile = uService.findUserById(1);
-// mav.addObject("userProfile", userProfile);
-// return mav;
-// }
-// }
