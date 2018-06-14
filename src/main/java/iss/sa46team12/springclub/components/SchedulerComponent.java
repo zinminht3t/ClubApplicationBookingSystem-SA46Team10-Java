aede214c14875da696f736fa79a0dd80b4810890
Jayden <wwj.jayden@gmail.com>
2018-06-15 01:34:10 +0800

    accidentally removed some of my codes :(

--- src/main/java/iss/sa46team12/springclub/components/SchedulerComponent.java
+++ src/main/java/iss/sa46team12/springclub/components/SchedulerComponent.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import iss.sa46team12.springclub.email.SendEmail;
 import iss.sa46team12.springclub.models.Subscription;
 import iss.sa46team12.springclub.models.User;
 import iss.sa46team12.springclub.repositories.SubscriptionRepository;
@@ -63,6 +64,8 @@ public class SchedulerComponent {
 				userService.editUser(u);
 				subscriptionService.editSub(sub);
 
+				SendEmail.sendEmail("spring12@gmail.com", u.getEmail(), "Spring Club - Account Expiry",
+						"Your account with this email has expired! Please register with us again!");
 			}
 		}
 
